176003729b5d7ef62468339522d32adeeaf76b39
aamgoulart <aamgoulart@outlook.com>
2021-02-21 16:26:59 -0300

    add endpoint delete

--- api/objects/student.php
+++ api/objects/student.php
@@ -70,28 +70,54 @@ class Student{
     }
 
     // used when filling up the update product form
-function readOne($id){
-  
-    // query to read single record
-    $query = "SELECT 
-        id_student, name, course, term
-    FROM
-        student
-    WHERE id_student = $1;
-";
+    function readOne($id){
+    
+        // query to read single record
+        $query = "SELECT 
+            id_student, name, course, term
+        FROM
+            student
+        WHERE id_student = $1;
+    ";
 
-    $parans = array (
-        "id_student" => $id,
-    );
+        $parans = array (
+            "id_student" => $id,
+        );
 
 
-    // execute query
-    $result = pg_query_params($this->conn, $query, $parans);
-  
-    // get retrieved row
-    $row = pg_fetch_all($result);
+        // execute query
+        $result = pg_query_params($this->conn, $query, $parans);
+    
+        // get retrieved row
+        $row = pg_fetch_all($result);
 
-    return $row;
-}
+        return $row;
+    }
+
+    // delete the product
+    function delete(){
+    
+        // delete query
+        $query = "DELETE FROM " . "student" . " WHERE id_student = $1";
+    
+        // sanitize
+        $this->id=htmlspecialchars(strip_tags($this->id));
+    
+        // bind id of record to delete
+
+        $parans = array (
+            "id_student" => $this->id,
+        );
+
+        // $stmt->bindParam(1, $this->id);
+        $result = pg_query_params($this->conn, $query, $parans);
+
+        // execute query
+        if($result){
+            return true;
+        }
+    
+        return false;
+    }
 }
 ?>
