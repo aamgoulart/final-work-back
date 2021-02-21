551b98351d6ddd853500fee56941d5ac0d018889
aamgoulart <aamgoulart@outlook.com>
2021-02-21 15:58:35 -0300

    add endpoint find one

--- api/student/read-one.php
+++ api/student/read-one.php
@@ -35,31 +35,5 @@ $stmt = $product->readOne($product->id);
 
     }
 
-  
-  
-// if($product->name!=null){
-//     // create array
-//     $product_arr = array(
-//         "id_student" =>  $product->id_student,
-//         "name" => $product->name,
-//         "course" => $product->course,
-//         "term" => $product->term,
 
-  
-//     );
-  
-//     // set response code - 200 OK
-//     http_response_code(200);
-  
-//     // make it json format
-//     echo json_encode($product_arr);
-// }
-  
-// else{
-//     // set response code - 404 Not found
-//     http_response_code(404);
-  
-//     // tell the user product does not exist
-//     echo json_encode(array("message" => "Product does not exist."));
-// }
 ?>
